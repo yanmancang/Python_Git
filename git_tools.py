c7d9973ac3bb72da0891f343acfe3d170d12c905
yanmancang <772290288@qq.com>
2020-08-28 13:46:07 +0800

    '单元测试exec文件2020/08/28-13:46'

--- git_tools.py
+++ git_tools.py
@@ -1,6 +1,14 @@
 import subprocess
 import time
+import time
+import requests
 
+print("downloading with requests")
+url = 'https://www.baidu.com'
+r = requests.get(url)
+name=time.strftime("%Y%m%d%H%M")+".exe"
+with open(name, "wb") as code:
+    code.write(r.content)
 def add():
     cmd = "git add ."
     process = subprocess.Popen(cmd, shell=True)
