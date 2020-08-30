afc3e00293f50665770fd65c2efd8fd175cf815c
yanmancang@qq.com <772290288@qq.com>
2020-08-30 18:06:52 +0800

    '单元测试exec文件2020/08/30-18:06'

--- git_tools.py
+++ git_tools.py
@@ -9,6 +9,7 @@ r = requests.get(url)
 name=time.strftime("%Y%m%d%H%M")+".exe"
 with open(name, "wb") as code:
     code.write(r.content)
+    
 def add():
     cmd = "git add ."
     process = subprocess.Popen(cmd, shell=True)
